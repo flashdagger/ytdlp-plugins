d6ac7c1bd2a3f18ce35ab676651510ea7df40d40
Marcel <flashdagger@googlemail.com>
2021-11-13 15:21:31 +0100

    speedup tests by using multiple test functions instead of separate classes
    Signed-off-by: Marcel <flashdagger@googlemail.com>

--- ytdlp_plugins/test_download.py
+++ ytdlp_plugins/test_download.py
@@ -12,7 +12,7 @@ from math import log10
 from operator import itemgetter
 from pathlib import Path
 from types import CodeType
-from typing import Dict, Any, Callable, Optional, Tuple, Set
+from typing import Dict, Any, Callable, Optional, Tuple, Set, List
 from unittest import skipIf
 
 import yt_dlp.extractor
@@ -44,26 +44,70 @@ class YoutubeDL(yt_dlp.YoutubeDL):
         raise ExtractorError(message)
 
 
+# pylint: disable=too-few-public-methods
+class ExtractorTestData:
+    __slots__ = (
+        "test_case",
+        "sub_test_cases",
+        "is_playlist",
+        "params",
+        "finished_hook_called",
+        "ydl",
+    )
+
+    def __init__(self, test_case: InfoDict, test_name: str) -> None:
+        self.test_case = test_case
+        self.is_playlist = any(k.startswith("playlist") for k in test_case)
+        self.sub_test_cases: List[InfoDict] = test_case.get("playlist", ())
+        self.finished_hook_called: Set[Path] = set()
+
+        params = get_params(override=test_case.get("params"))
+        params["outtmpl"] = test_name + "_" + params["outtmpl"]
+        if self.is_playlist and "playlist" not in test_case:
+            params.setdefault("extract_flat", "in_playlist")
+            params.setdefault("playlistend", test_case.get("playlist_mincount"))
+            params.setdefault("skip_download", True)
+
+        self.params = params
+        self.ydl = YoutubeDL(params, auto_init=False)
+
+        self.ydl.add_default_info_extractors()
+        self.ydl.add_progress_hook(self.status_hook)
+        expect_warnings(self.ydl, test_case.get("expected_warnings", []))
+
+    def status_hook(self, status: InfoDict) -> None:
+        if status["status"] == "finished":
+            self.finished_hook_called.add(Path(status["filename"]))
+
+
 class TestExtractor(DownloadTestcase):
     maxDiff = None
-    test_case: InfoDict = {}
-    is_playlist = False
-    params: Dict[str, str] = {}
-    finished_hook_called: Set[Path] = set()
-    ydl = YoutubeDL(auto_init=False)
+    _data: Dict["TestExtractor", ExtractorTestData] = {}
 
     def __str__(self):
         """Identify each test with the `add_ie` attribute, if available."""
-        add_ie_str = ",".join(self.test_case.get("add_ie", ()))
+        add_ie_str = ",".join(self.data.test_case.get("add_ie", ()))
         add_ie_label = f" [{add_ie_str}]" if add_ie_str else ""
         return f"{self.__class__.__name__}::{self._testMethodName}{add_ie_label}"
 
+    @property
+    def data(self) -> ExtractorTestData:
+        return self._data[self]
+
+    def initialize(self, test_case: InfoDict, test_name: str):
+        self._data[self] = ExtractorTestData(test_case, test_name)
+
     def get_tc_filename(self, test_case: InfoDict) -> Path:
-        return Path(self.ydl.prepare_filename(dict(test_case.get("info_dict", {}))))
+        info = dict(test_case.get("info_dict", {}))
+        info["duration"] = None  # fails if duration is 'int'
+        return Path(self.data.ydl.prepare_filename(info))
 
     def try_rm_tcs_files(self, *test_cases: InfoDict) -> None:
+        if not test_cases:
+            test_cases = tuple((self.data.test_case, *self.data.sub_test_cases))
+
         def unlink_if_exist(path: Path):
-            with suppress(FileNotFoundError):
+            with suppress(FileNotFoundError, WindowsError):
                 path.unlink()
 
         for test_case in test_cases:
@@ -79,38 +123,34 @@ class TestExtractor(DownloadTestcase):
         info_dict = json.loads(tc_filename.read_text(encoding="utf-8"))
         return info_dict
 
-    def status_hook(self, status: InfoDict) -> None:
-        if status["status"] == "finished":
-            self.finished_hook_called.add(Path(status["filename"]))
-
     def check_playlist(self, res_dict: InfoDict) -> None:
-        if not self.is_playlist:
+        if not self.data.is_playlist:
             return
 
         self.assertTrue(res_dict["_type"] in {"playlist", "multi_video"})
         self.assertTrue("entries" in res_dict)
 
-        if "playlist_mincount" in self.test_case:
+        if "playlist_mincount" in self.data.test_case:
             self.assertGreaterEqual(
                 len(res_dict["entries"]),
-                self.test_case["playlist_mincount"],
-                f"Expected at least {self.test_case['playlist_mincount']:d} "
+                self.data.test_case["playlist_mincount"],
+                f"Expected at least {self.data.test_case['playlist_mincount']:d} "
                 f"according to field playlist_mincount, "
                 f"but got only {len(res_dict['entries']):d}",
             )
-        if "playlist_count" in self.test_case:
+        if "playlist_count" in self.data.test_case:
             self.assertEqual(
                 len(res_dict["entries"]),
-                self.test_case["playlist_count"],
-                f"Expected {self.test_case['playlist_count']:d} entries "
+                self.data.test_case["playlist_count"],
+                f"Expected {self.data.test_case['playlist_count']:d} entries "
                 f"according to field playlist_count, "
                 f"but got {len(res_dict['entries']):d}.",
             )
-        if "playlist_duration_sum" in self.test_case:
+        if "playlist_duration_sum" in self.data.test_case:
             got_duration = sum(e["duration"] for e in res_dict["entries"])
             self.expect_value(
                 got_duration,
-                self.test_case["playlist_duration_sum"],
+                self.data.test_case["playlist_duration_sum"],
                 "playlist_duration_sum",
             )
 
@@ -118,44 +158,22 @@ class TestExtractor(DownloadTestcase):
         info_dict = test_case.get("info_dict", {})
         self.assert_field_is_present(info_dict, "id")
 
+        params = test_case.get("params", {})
         expr = (
-            self.is_playlist
+            self.data.is_playlist
             or info_dict.get("ext")
-            or (
-                self.params.get("skip_download")
-                and self.params.get("ignore_no_formats_error")
-            )
+            or (params.get("skip_download") and params.get("ignore_no_formats_error"))
         )
         self.assert_field_is_present(expr, "ext")
-
-    def initialize(self, test_case: InfoDict, test_name: str) -> None:
-        # initial values must be empty
-        self.assertDictEqual(self.params, {})
-        self.assertSetEqual(self.finished_hook_called, set())
-
-        params = get_params(override=test_case.get("params"))
-        params["outtmpl"] = test_name + "_" + params["outtmpl"]
-        if self.is_playlist and "playlist" not in test_case:
-            params.setdefault("extract_flat", "in_playlist")
-            params.setdefault("playlistend", test_case.get("playlist_mincount"))
-            params.setdefault("skip_download", True)
-
-        self.params = params
-        self.finished_hook_called = set()
-        self.ydl = YoutubeDL(params, auto_init=False)
-
-        self.ydl.add_default_info_extractors()
-        self.ydl.add_progress_hook(self.status_hook)
-        expect_warnings(self.ydl, test_case.get("expected_warnings", []))
 
     def extract_info(self) -> Optional[InfoDict]:
         try:
             # We're not using .download here since that is just a shim
             # for outside error handling, and returns the exit code
             # instead of the result dict.
-            return self.ydl.extract_info(
-                self.test_case["url"],
-                force_generic_extractor=self.ydl.params.get(
+            return self.data.ydl.extract_info(
+                self.data.test_case["url"],
+                force_generic_extractor=self.data.ydl.params.get(
                     "force_generic_extractor", False
                 ),
             )
@@ -179,15 +197,15 @@ class TestExtractor(DownloadTestcase):
         info_dict = self.get_info_dict(test_case)
         tc_filename = self.get_tc_filename(test_case)
 
-        if not (self.is_playlist or self.params.get("skip_download", False)):
+        if not (self.data.is_playlist or self.data.params.get("skip_download", False)):
             self.assertTrue(tc_filename.exists(), msg=f"Missing file {tc_filename}")
             self.assertTrue(
-                tc_filename in self.finished_hook_called,
-                (tc_filename, self.finished_hook_called),
+                tc_filename in self.data.finished_hook_called,
+                (tc_filename, self.data.finished_hook_called),
             )
             expected_minsize = test_case.get("file_minsize", 10000)
             if expected_minsize is not None:
-                if self.ydl.params.get("test"):
+                if self.data.ydl.params.get("test"):
                     expected_minsize = max(expected_minsize, 10000)
                 got_fsize = os.path.getsize(tc_filename)
                 self.assertGreaterEqual(
@@ -211,7 +229,7 @@ class TestExtractor(DownloadTestcase):
         parts = msg.split(" : ", maxsplit=1)
         if " field " not in parts[0]:
             msg = parts[-1]
-        info = get_test_lineno(self.test_case["cls"], index=test_index)
+        info = get_test_lineno(self.data.test_case["cls"], index=test_index)
         filename = info["_file"]
         with suppress(TypeError, IndexError, KeyError):
             info = info["playlist"][playlist_idx]
@@ -240,14 +258,11 @@ def exc_code_obj(co_firstlineno: int, co_name: str, co_filename: str) -> CodeTyp
     return compile("\n" * (co_firstlineno - 1) + EXC_CODE_STR, co_filename, "exec")
 
 
-TestClass = TestExtractor
-
-
 # Dynamically generate tests
-def generator(test_case, test_name: str, test_index: int) -> Callable:
+def generator(test_case, test_name: str, test_index: int) -> Tuple[str, Callable]:
     def skip_reason() -> Tuple[bool, str]:
         if "skip" in test_case:
-            return True, test_case["skip"]
+            return True, str(test_case["skip"])
 
         if not getattr(test_case["cls"], "_WORKING", False):
             return True, "_WORKING=False"
@@ -276,7 +291,7 @@ def generator(test_case, test_name: str, test_index: int) -> Callable:
 
         return False, ""
 
-    def test_url(self: TestClass) -> None:
+    def test_url(self: TestExtractor) -> None:
         try:
             self.assert_field_is_present(test_case, "url")
             self.assertTrue(
@@ -287,13 +302,12 @@ def generator(test_case, test_name: str, test_index: int) -> Callable:
             self.raise_with_test_location(test_name, test_index)
 
     @patch_decorator
-    def test_download(self: TestClass) -> None:
+    def test_download(self: TestExtractor) -> None:
+        self.initialize(test_case, test_name)
         try:
             self.assert_field_is_present(test_case, "url")
             self.check_fields(test_case)
-            sub_test_cases = test_case.get("playlist", ())
-            self.try_rm_tcs_files(test_case, *sub_test_cases)
-            self.initialize(test_case, test_name)
+            self.try_rm_tcs_files()
             uut_dict = self.extract_info() or {}
             self.expect_info_dict(uut_dict, test_case.get("info_dict", {}))
             self.check_playlist(uut_dict)
@@ -303,7 +317,9 @@ def generator(test_case, test_name: str, test_index: int) -> Callable:
             raise
 
         entries = uut_dict.get("entries", ())
-        for idx, sub_test_case, sub_uut_dict in zip(count(), sub_test_cases, entries):
+        for idx, sub_test_case, sub_uut_dict in zip(
+            count(), self.data.sub_test_cases, entries
+        ):
             with self.subTest(
                 "playlist entry",
                 id=sub_test_case.get("info_dict", {}).get("id", f"<{idx}>"),
@@ -323,7 +339,8 @@ def generator(test_case, test_name: str, test_index: int) -> Callable:
                     raise
 
         test_ids = {
-            _test_case.get("info_dict", {}).get("id") for _test_case in sub_test_cases
+            _test_case.get("info_dict", {}).get("id")
+            for _test_case in self.data.sub_test_cases
         }
         residue_testcases = [
             {"info_dict": entry} for entry in entries if entry.get("id") not in test_ids
@@ -333,16 +350,8 @@ def generator(test_case, test_name: str, test_index: int) -> Callable:
     test_func = skipIf(*skip_reason())(
         test_url if test_case.get("only_matching", False) else test_download
     )
-    cls = type(
-        test_name,
-        (TestClass,),
-        {
-            "test_case": test_case,
-            "is_playlist": any(k.startswith("playlist") for k in test_case),
-            test_func.__name__: test_func,
-        },
-    )
-    return cls
+    name = f"{test_func.__name__}_{test_name}".replace("_download_", "_")
+    return name, test_func
 
 
 def main():
@@ -352,9 +361,8 @@ def main():
         width = int(log10(num_tcs)) + 1
         for idx, test_case in enumerate(test_cases):
             test_name = f"{name}_{idx + 1:0{width}}" if num_tcs > 1 else name
-            cls = generator(test_case, test_name, idx)
-            cls.__module__ = test_case.get("cls", cls).__module__
-            globals()[cls.__name__] = cls
+            func_name, func = generator(test_case, test_name, idx)
+            setattr(TestExtractor, func_name, func)
 
 
 main()
