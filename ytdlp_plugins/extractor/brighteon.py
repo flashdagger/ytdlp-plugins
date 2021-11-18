7b2ce0cfecfaf670808db580a573fb30ab20ef0a
Marcel <flashdagger@googlemail.com>
2021-11-18 20:25:26 +0100

    Update version 2021.11.17 → 2021.11.18

--- ytdlp_plugins/extractor/brighteon.py
+++ ytdlp_plugins/extractor/brighteon.py
@@ -18,7 +18,7 @@ from yt_dlp.utils import (
 )
 from ytdlp_plugins.utils import estimate_filesize, ParsedURL
 
-__version__ = "2021.11.17"
+__version__ = "2021.11.18"
 
 
 # pylint: disable=abstract-method
