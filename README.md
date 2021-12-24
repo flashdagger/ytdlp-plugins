2db6a781f6a0d91cd08f8b901484b3150032061a
Marcel <flashdagger@googlemail.com>
2021-12-25 00:16:19 +0100

    initial commit
    Signed-off-by: Marcel <flashdagger@googlemail.com>

---
+++ README.md
@@ -0,0 +1,28 @@
+# dtube.tv support for yt-dlp
+
+## installation
+
+You can install ytdlp-dtube via pip:
+
+* Use [PyPI package](https://pypi.org/project/yt-dlp):
+
+  `python3 -m pip install --upgrade ytdlp-dtube`
+* Install from Github branch:
+
+  `python3 -m pip install -U https://github.com/flashdagger/ytdlp-plugins/archive/refs/heads/dtube.zip`
+
+Note that on some systems, you may need to use `py` or `python` instead of `python3`
+
+## example: show the 20 recent titles from channel
+
+`python3 -m ytdlp_plugins -e --playlist-items 1-20 https://dtube.tv/videos`
+
+## yt-dlp
+
+yt-dlp is a [youtube-dl](https://github.com/ytdl-org/youtube-dl) fork based on the now
+inactive [youtube-dlc](https://github.com/blackjack4494/yt-dlc). The main focus of this project is adding new features
+and patches while also keeping up to date with the original project
+
+Note that **all** plugins are imported even if not invoked, and that **there are no checks** performed on plugin code.
+Use plugins at your own risk and only if you trust the code
+
---
+++ ytdlp_plugins/extractor/dtube.py
@@ -0,0 +1,178 @@
+# coding: utf-8
+import json
+
+from yt_dlp.extractor.common import InfoExtractor
+from yt_dlp.utils import (
+    parse_iso8601,
+    traverse_obj,
+    UnsupportedError,
+    int_or_none,
+    HEADRequest,
+)
+
+__version__ = "2021.11.28"
+
+
+# pylint: disable=abstract-method
+class DTubeIE(InfoExtractor):
+    _VALID_URL = r"""(?x)
+                    https?://(?:www\.)?d\.tube/
+                    (?:\#!/)?v/
+                    (?P<uploader_id>[0-9a-z.-]+)/
+                    (?P<id>\w+)
+                    """
+    _TEST = {
+        "url": "https://d.tube/v/famigliacurione/QmUJquzf7DALjwUExoHtTjS8PgGqfGohzMr4W3XJ56q9pR",
+        "md5": "4ad5272197655dc033bfd0cc039b71f2",
+        "info_dict": {
+            "id": "famigliacurione/QmUJquzf7DALjwUExoHtTjS8PgGqfGohzMr4W3XJ56q9pR",
+            "title": "La Prova by SOSO",
+            "description": "md5:c942bfe98693a2e81510464d10869449",
+            "ext": "mp4",
+            "thumbnail": "https://cdn.steemitimages.com/"
+            "DQmbCfaJkTRrjerNhcyDCoviBqpXB8ZDh31NhQPWvcyEj6U/laprovathumb.jpg",
+            "tags": ["music", "hiphop"],
+            "duration": 71,
+            "uploader_id": "famigliacurione",
+            "upload_date": "20211223",
+            "timestamp": 1640297596.628,
+        },
+        "params": {
+            "format": "480p",
+        },
+    }
+
+    def formats(self, files):
+        url_map = {
+            "ipfs": ["https://player.d.tube/ipfs", "https://ipfs.d.tube/ipfs"],
+            "btfs": ["https://player.d.tube/btfs"],
+        }
+
+        for provider, base_urls in url_map.items():
+            if provider in files and "vid" in files[provider]:
+                break
+        else:
+            return []
+
+        formats = []
+        res_map = {}
+        for format_id, content_id in files[provider].get("vid", {}).items():
+            for candidate in base_urls:
+                head_response = self._request_webpage(
+                    HEADRequest(f"{candidate}/{content_id}"),
+                    None,
+                    note=False,
+                    errnote=f"skipping {candidate!r}",
+                    fatal=False,
+                )
+                if head_response is False:
+                    continue
+                fsize = int_or_none(head_response.headers["Content-Length"])
+                height = int_or_none(format_id)
+                if height:
+                    res_map[fsize] = height
+                formats.append(
+                    {
+                        "format_id": f"{height}p" if height else format_id,
+                        "url": f"{candidate}/{content_id}",
+                        "height": height or res_map.get(fsize),
+                        "filesize": fsize,
+                        "ext": head_response.headers["Content-Type"].split("/")[-1],
+                    }
+                )
+                base_urls = [candidate]
+                break
+        return formats
+
+    def avalon_api(self, video_id):
+        result = self._download_json(
+            f"https://avalon.d.tube/content/{video_id}",
+            video_id,
+            note="Downloading avalon metadata",
+            fatal=False,
+        )
+        if result is False:
+            return None
+        with open("content.json", "w") as fd:
+            json.dump(result, fd, indent=4)
+        info = result["json"]
+        video_provider = info.get("files", {})
+        if "youtube" in video_provider:
+            redirect_url = video_provider["youtube"]
+        elif "dailymotion" in video_provider:
+            redirect_url = (
+                f"https://www.dailymotion.com/video/{video_provider['dailymotion']}"
+            )
+        elif "url" in info:
+            redirect_url = info["url"]
+        else:
+            redirect_url = None
+        if redirect_url:
+            return {
+                "_type": "url",
+                "title": info.get("title"),
+                "url": redirect_url,
+            }
+        timestamp = result.get("ts")
+
+        return {
+            "id": video_id,
+            "title": info.get("title"),
+            "description": info.get("desc"),
+            "thumbnail": info.get("thumbnailUrl"),
+            "tags": list(result.get("tags", {}).keys()),
+            "duration": int_or_none(info.get("dur")),
+            "formats": self.formats(info.get("files", {})),
+            "timestamp": timestamp and timestamp * 1e-3,
+            "uploader_id": result.get("author"),
+        }
+
+    def steemit_api(self, video_id):
+        result = self._download_json(
+            "https://api.steemit.com/",
+            video_id,
+            note="Downloading steemit metadata",
+            data=json.dumps(
+                {
+                    "jsonrpc": "2.0",
+                    "method": "call",
+                    "params": ["condenser_api", "get_state", [f"/dtube/@{video_id}"]],
+                }
+            ).encode(),
+        )
+        content = traverse_obj(result, ("result", "content", video_id))
+        if content is None:
+            return None
+        with open("content.json", "w") as fd:
+            json.dump(content, fd, indent=4)
+        metadata = json.loads(content["json_metadata"])
+        with open("metadata.json", "w") as fd:
+            json.dump(metadata, fd, indent=4)
+
+        formats = []
+
+        return {
+            "id": video_id,
+            "title": content.get("title"),
+            "description": traverse_obj(
+                metadata, "video/content/description".split("/"), default=None
+            ),
+            # "thumbnail": canonical_url(info.get("snaphash")),
+            "tags": traverse_obj(metadata, "video/content/tags".split("/"), default=[]),
+            "duration": traverse_obj(
+                metadata, "video/info/duration".split("/"), default=None
+            ),
+            "formats": formats,
+            "timestamp": parse_iso8601(content.get("created")),
+            "uploader_id": content.get("author"),
+        }
+
+    def _real_extract(self, url):
+        video_id = "/".join(self._match_valid_url(url).groups())
+        print("id:", video_id)
+        for api in (self.avalon_api, self.steemit_api):
+            info = api(video_id)
+            if info:
+                return info
+
+        raise UnsupportedError(url)
