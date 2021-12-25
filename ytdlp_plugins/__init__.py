f21f30a7637af41c59733f207356b206c2451f0e
Marcel <flashdagger@googlemail.com>
2021-12-25 21:04:04 +0100

    Update version 2021.11.19 → 2021.12.25

--- ytdlp_plugins/__init__.py
+++ ytdlp_plugins/__init__.py
@@ -21,7 +21,7 @@ from yt_dlp.extractor.common import InfoExtractor
 from .generic import GenericIE
 from .utils import unlazify
 
-__version__ = "2021.11.19"
+__version__ = "2021.12.25"
 
 
 _INITIALIZED = False
