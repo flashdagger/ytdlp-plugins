93f5b74bf83a68c7fc82ec35e618a2d8f678e410
Marcel <flashdagger@googlemail.com>
2021-12-28 13:40:44 +0100

    Update version 2021.12.23 → 2021.12.28

--- ytdlp_plugins/extractor/servustv.py
+++ ytdlp_plugins/extractor/servustv.py
@@ -14,7 +14,7 @@ from yt_dlp.utils import (
 )
 from ytdlp_plugins.utils import estimate_filesize, ParsedURL
 
-__version__ = "2021.12.23"
+__version__ = "2021.12.28"
 
 
 class ServusTVIE(InfoExtractor):
