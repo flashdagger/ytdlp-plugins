fba88db2db939fc2d2210101e915a53906ac9f9c
Marcel <flashdagger@googlemail.com>
2022-05-06 19:47:45 +0200

    use payload.js extraction as fallback for single videos
    Signed-off-by: Marcel <flashdagger@googlemail.com>

--- ytdlp_plugins/extractor/auf1.py
+++ ytdlp_plugins/extractor/auf1.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import json
 import re
 import time
 from urllib.error import HTTPError
@@ -12,6 +13,9 @@ from yt_dlp.utils import (
     clean_html,
     traverse_obj,
     UnsupportedError,
+    urljoin,
+    base_url,
+    js_to_json,
 )
 
 __version__ = "2022.04.10"
@@ -50,22 +54,25 @@ class Auf1IE(InfoExtractor):
                 "categories": ["News & Politics"],
             },
             "params": {"skip_download": True, "nocheckcertificate": True},
-            "expected_warnings": ["Retrying due to too many requests."],
+            "expected_warnings": [
+                "Retrying due to too many requests.",
+                "The read operation timed out",
+            ],
         },
         {  # JSON API without payload.js
             "url": "https://auf1.tv/stefan-magnet-auf1/"
             "heiko-schoening-chaos-und-krieg-gehoeren-leider-zu-deren-plan/",
             "info_dict": {
-                "id": "wyfEHcF2GAsXeu3htchE9H",
+                "id": "dVk8Q3VNMLi7b7uhyuSSp6",
                 "ext": "mp4",
                 "title": "Heiko Schöning: „Chaos und Krieg gehören leider zu deren Plan“",
                 "description": "md5:6fb9e7eb469fc544223018a2ff3c998c",
-                "timestamp": 1646672419,
-                "uploader": "redaktion",
-                "uploader_id": "3",
+                "timestamp": int,
+                "uploader": str,
+                "uploader_id": str,
                 "upload_date": "20220307",
-                "channel": "auf1.tv",
-                "channel_url": "https://auf1.eu/video-channels/auf1.tv",
+                "channel": str,
+                "channel_url": "contains:/video-channels/auf1.tv",
                 "duration": 2089,
                 "view_count": int,
                 "like_count": int,
@@ -74,7 +81,10 @@ class Auf1IE(InfoExtractor):
                 "categories": ["News & Politics"],
             },
             "params": {"skip_download": True, "nocheckcertificate": True},
-            "expected_warnings": ["Retrying due to too many requests."],
+            "expected_warnings": [
+                "Retrying due to too many requests.",
+                "The read operation timed out",
+            ],
         },
         {
             # playlist for category
@@ -82,11 +92,14 @@ class Auf1IE(InfoExtractor):
             "info_dict": {
                 "id": "nachrichten-auf1",
                 "title": "Nachrichten AUF1",
-                "description": "md5:2b1c09d2134e9bf317c9bcf078c14da8",
+                "description": "md5:42259265c58a49eb7b103d3540a06715",
             },
             "params": {"skip_download": True},
             "playlist_mincount": 300,
-            "expected_warnings": ["Retrying due to too many requests."],
+            "expected_warnings": [
+                "Retrying due to too many requests.",
+                "The read operation timed out",
+            ],
         },
         {
             # playlist for all videos
@@ -188,16 +201,39 @@ class Auf1IE(InfoExtractor):
             **kwargs,
         )
 
+    def _payloadjs(self, url, page_id):
+        webpage = self._download_webpage(url, page_id)
+        payloadjs_url = self._search_regex(
+            r'href="([^"]+/payload.js)"', webpage, "payload url"
+        )
+        payloadjs_url = urljoin(base_url(url), payloadjs_url)
+        payload_js = self._download_webpage(
+            payloadjs_url, page_id, note="Downloading payload.js"
+        )
+        metadata = self._search_regex(r"{ *return ({.+}).*}", payload_js, "payloadjs")
+        js_string = js_to_json(metadata, vars={"a": f'"{page_id}"', "b": "null"})
+        return json.loads(js_string)
+
+    def _metadata(self, url, *, page_id, method="api"):
+
+        if method == "api":
+            return self.call_with_retries(
+                lambda: self.call_api(f"getContent/{page_id}", page_id),
+                http_error_map={500: UnsupportedError(url)},
+            )
+        payload = self._payloadjs(url, page_id)
+        return payload["data"][0]["payload"]
+
     def _real_extract(self, url):
         category, page_id = self._match_valid_url(url).groups()
-        http_error_map = {500: UnsupportedError(url)}
 
         # single video
         if category:
-            metadata = self.call_with_retries(
-                lambda: self.call_api(f"getContent/{page_id}", page_id),
-                http_error_map=http_error_map,
-            )
+            try:
+                metadata = self._metadata(url, page_id=page_id, method="api")
+            except ExtractorError as exc:
+                self.report_warning(exc, page_id)
+                metadata = self._metadata(url, page_id=page_id, method="payloadjs")
             peertube_url = self.parse_url(metadata.get("videoUrl"))
             peertube_info = (
                 self.peertube_extract(peertube_url) if peertube_url else None
@@ -216,7 +252,7 @@ class Auf1IE(InfoExtractor):
 
         metadata = self.call_with_retries(
             lambda: self.call_api(f"getShow/{page_id}", page_id),
-            http_error_map=http_error_map,
+            http_error_map={500: UnsupportedError(url)},
         )
 
         return self.playlist_from_entries(
