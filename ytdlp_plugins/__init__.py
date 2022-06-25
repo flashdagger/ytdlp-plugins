b344542b0327876c9e33f9afde797a2988448791
Marcel <flashdagger@googlemail.com>
2022-06-26 00:57:15 +0200

    Update version 2022.04.10 → 2022.06.25

--- ytdlp_plugins/__init__.py
+++ ytdlp_plugins/__init__.py
@@ -22,7 +22,7 @@ from yt_dlp.extractor.common import InfoExtractor
 from .generic import GenericIE
 from .utils import unlazify
 
-__version__ = "2022.04.10"
+__version__ = "2022.06.25"
 
 
 _INITIALIZED = False
